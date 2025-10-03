55790078a0906fab5e1a97f53b8b6af254a085a1
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-10-03 14:54:15 +0530

    added workspace models

--- internal/models/workspace.go
+++ internal/models/workspace.go
@@ -6,7 +6,7 @@ type Workspace struct {
 	ID          bson.ObjectID   `bson:"_id,omitempty" json:"id"`
 	Name        string          `bson:"name" json:"name"`
 	Description string          `bson:"description" json:"description"`
-	OwnerID     bson.ObjectID   `bson:"owner_id" json:"ownerId"`
+	Owner       bson.ObjectID   `bson:"owner_id" json:"ownerId"`
 	Members     []bson.ObjectID `bson:"members" json:"members"`
 	CreatedAt   string          `bson:"created_at" json:"createdAt"`
 }
